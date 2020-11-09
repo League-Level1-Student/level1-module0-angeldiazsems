19e42677cebf143cd9e7570b8d53c7305007a831
root <root@e3eb27881861>
2020-11-09 21:27:44 +0000

    level 1 :) module 0 :(

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,28 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
 		
-	}
+		Robot houserobot = new Robot();
+		houserobot.setSpeed(10);
+		houserobot.setX(-10);
+		houserobot.setY(500);
+		houserobot.penDown();
+		houserobot.turn(90);
+		houserobot.move(50);
+		houserobot.turn(-90);
+		for(int i = 0;i<3; i++) {
+		houserobot.move(75);
+		houserobot.turn(90);
+		houserobot.turn(270);
+		houserobot.turn(90);
+		}
+		
+		
+		
+		
+		}
+		
 }
