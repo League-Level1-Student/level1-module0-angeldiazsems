7372d570446492cb7231800f5fc282a47e242b27
root <root@0cca7e69d50a>
2020-11-30 21:26:44 +0000

    house is complete

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -9,7 +9,7 @@ public class Houses {
 	public void run() {
 
 		Random ran = new Random();
-		
+
 		houserobot.penDown();
 		houserobot.setSpeed(75);
 		houserobot.setX(-10);
@@ -19,35 +19,28 @@ public class Houses {
 		houserobot.turn(-90);
 		for (int i = 0; i < 10; i++) {
 			int color = ran.nextInt(5);
-			
-			if(color%5==0) {
+
+			if (color % 5 == 0) {
 				houserobot.setPenColor(Color.BLUE);
-			}
-			else if(color%5==1) {
+			} else if (color % 5 == 1) {
 				houserobot.setPenColor(Color.RED);
-			}
-			else if(color%5==2) {
+			} else if (color % 5 == 2) {
 				houserobot.setPenColor(Color.GREEN);
-			}
-			else if(color%5==3) {
+			} else if (color % 5 == 3) {
 				houserobot.setPenColor(Color.PINK);
-			}
-			else if(color%5==4) {
+			} else if (color % 5 == 4) {
 				houserobot.setPenColor(Color.CYAN);
 			}
-			
-			
-			
+
 			int height = ran.nextInt(3);
-if(height%3==0) {
-	houseDrawer("small");
-}
-else if(height%3==1) {
-	houseDrawer("medium");
-}
-else if(height%3==2)	{
-	houseDrawer("large");
-}
+			if (height % 3 == 0) {
+				houseDrawer("small");
+			} else if (height % 3 == 1) {
+				houseDrawer("medium");
+			} else if (height % 3 == 2) {
+				houseDrawer("large");
+			}
+			  
 
 		}
 	}
@@ -56,28 +49,47 @@ else if(height%3==2)	{
 
 	void houseDrawer(String height) {
 		int hi = 123;
-		
-	if(height.equals("small")) {
+
+		if (height.equals("small")) {
 			hi = 60;
 		}
-	
-	else if(height.equals("medium")) {
-		hi = 120;
-	}
-		
-	else if(height.equals("large")){
-		hi=240;
-	}
-		
+
+		else if (height.equals("medium")) {
+			hi = 120;
+		}
+
+		else if (height.equals("large")) {
+			hi = 240;
+		}
 		// starts facing up
 		houserobot.move(hi);
-		houserobot.turn(90);
-		houserobot.move(50);
-		houserobot.turn(90);
+		if(height.equals("large")) {
+			drawFlatRoof();
+		}
+		else {
+			drawPointyRoof();
+		}
 		houserobot.move(hi);
 		houserobot.turn(-90);
 		houserobot.move(35);
 		houserobot.turn(-90);
+	
+	}
+void drawFlatRoof() {
+	houserobot.turn(90);
+	houserobot.move(50);
+	houserobot.turn(90);
+}
+	 void drawPointyRoof() {
+		
+		houserobot.turn(45);
+		houserobot.move(50);
+		houserobot.turn(90);
+		houserobot.move(50);
+		houserobot.turn(45);
+		
+		
+		
 
 	}
 
