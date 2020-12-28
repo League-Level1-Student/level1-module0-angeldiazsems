d019999a5b43864b1a929b3a429def763fc9ea1f
root <root@e5cef2656312>
2020-12-28 21:26:48 +0000

    drumz

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -16,6 +16,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class DrumKit implements MouseListener {
@@ -42,28 +43,29 @@ frame.add(panel);
 		// Eclipse project under "recipe package(drum_kit)".
 
 		// 8. Put the name of your image file in a String variable.
-String drum = "cymbal.png";
+String drum = "cymbal.jpg";
 		// 9. Edit the next line to use your String variable
-		 drumLabelWithImage = createLabelImage(drumImageString);
+		 drumLabelWithImage = createLabelImage(drum);
 
 		// 10. Add the image to the panel
-
+panel.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+new GridLayout();
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+frame.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+drumLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
+String anotherdrum = "snare.jpg";
 
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
-
+		System.out.println("mouse clicked");
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
 														// clicked on
@@ -73,10 +75,17 @@ String drum = "cymbal.png";
 		// leagueofamazing/code4life.
 
 		// 16. If they clicked on the drumImage...
-
+if(drumClicked == drumLabelWithImage){
+	JOptionPane.showMessageDialog(null, "BAM");
+}
 		// 17. ...use the playSound method to play a drum sound. Test to see if
 		// it works
 
+	}
+
+	private void syso (String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private JLabel createLabelImage(String fileName) throws MalformedURLException {
