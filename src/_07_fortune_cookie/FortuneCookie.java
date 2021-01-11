9947bf0368d8010dd85b5983851e09693eec517e
student <student@ip-172-31-63-187.us-west-2.compute.internal>
2021-01-11 13:26:34 -0800

    lots of work 2day

---
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -0,0 +1,14 @@
+package _07_fortune_cookie;
+
+	public class FortuneCookie {
+        public void showButton() {
+            System.out.println("Button clicked");
+       JFrame frame = new JFrame();
+        }
+
+	frame.setVisible(true);
+	
+	
+	
+	}
+
